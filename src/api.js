e01399734e094bc5aacb0d3be7279f87aa3e7821
Pradeep Pathak <pradeeppathak@Pradeeps-MacBook-Air.local>
2026-02-24 11:14:12 +0530

    use env based api url

--- src/api.js
+++ src/api.js
@@ -1,18 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://127.0.0.1:8000/api/",
-  
-//   // process.env.REACT_APP_API_BASE_URL,
-// });
-
-// export default API;
-
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
